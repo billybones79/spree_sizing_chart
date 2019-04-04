1bbf6432849cb53ec8c9b635015801abbadafdb6
Quantumphilisp <quantumphilisp@gmail.com>
2019-04-04 12:57:46 -0400

    initial solidus

--- app/overrides/add_sizing_chart_to_admin_submenu.rb
+++ app/overrides/add_sizing_chart_to_admin_submenu.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
-    virtual_path: 'spree/admin/shared/sub_menu/_product',
+    virtual_path: 'spree/admin/shared/_product_sub_menu',
     name: 'add_sizing_chart_to_admin_submenu',
     insert_bottom: "[data-hook=admin_product_sub_tabs]",
     text: "<%= tab :sizing_charts %>
--- config/initializers/spree_editor.rb
+++ config/initializers/spree_editor.rb
@@ -1,4 +1,8 @@
+Spree::Config[:show_raw_product_description] = true
+
 SpreeEditor::Config.tap do |config|
+  config.ids = 'product_description page_body event_body template_chart_chart_table sizing_chart_chart_table'
+
   # change the editor to CKEditor
   config.current_editor = 'CKEditor'
 end
