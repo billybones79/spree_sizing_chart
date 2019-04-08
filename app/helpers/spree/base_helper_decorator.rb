7a6fbe46b311c1e501a24bb86883c6d3fca007e1
Quantumphilisp <quantumphilisp@gmail.com>
2019-04-08 10:22:47 -0400

    fixed nested sets

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -2,12 +2,11 @@ Spree::BaseHelper.class_eval do
   def get_sizing_chart(product)
     brand = product.taxons.where(taxonomy_id: Rails.configuration.brand_id).pluck(:id)
     taxons = product.taxons.where(taxonomy_id: Rails.configuration.category_id).map(&:self_and_ancestors).flatten.uniq
-
-    chart = Spree::SizingChart.where("taxon_id IN (?) AND brand_id IN (?)", taxons, brand).joins(:taxon).order("spree_taxons.rgt")
-
+    chart = Spree::SizingChart.where("taxon_id IN (?) AND brand_id IN (?)", taxons, brand).joins(:taxon).order("spree_taxons.lft")
+    chart.empty? ? chart = Spree::SizingChart.where("taxon_id IN (?) AND brand_id = (?)", taxons, Rails.configuration.brand_id).joins(:taxon).order("spree_taxons.lft") : nil
     return nil if chart.empty?
 
-    chart
+    chart.last
 
   end
 end
