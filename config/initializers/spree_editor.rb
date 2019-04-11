dfadc21ba370add6b20a06be28ebd4d63060bedc
Quantumphilisp <quantumphilisp@gmail.com>
2019-04-11 16:04:54 -0400

    ajouter des config ids pour ckeditor pour pas avoir a le faire manuellement au cas ou

--- config/initializers/spree_editor.rb
+++ config/initializers/spree_editor.rb
@@ -1,4 +1,5 @@
 SpreeEditor::Config.tap do |config|
+  config.ids = 'product_description page_body event_body template_chart_chart_table sizing_chart_chart_table'
   # change the editor to CKEditor
   config.current_editor = 'CKEditor'
 end
